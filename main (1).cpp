e3e414c591583c55bb514e1113d0ea080c44273f
ShikshaSingh9581 <153091897+ShikshaSingh9581@users.noreply.github.com>
2023-12-14 17:25:20 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,18 @@
+//write a program to find the given number is positive or negative
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    if(n>0)
+    {
+        cout<<"the number is Positive";
+    }
+    else
+    {
+        cout<< "the number is Negative";
+    }
+    return 0;
+}
